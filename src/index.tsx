4cf47b0dd1227e8bd94190e9c3edc441c8c46d91
Joel Adria <2769376+joeladria@users.noreply.github.com>
2021-01-09 22:17:02 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import App from "./App";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
